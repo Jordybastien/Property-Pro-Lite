85e74a79ef114afc1f51f865a329b5e8dc1f1bed
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2019-07-12 22:31:38 +0000

    Error on flag

--- server/models/Flag.js
+++ server/models/Flag.js
@@ -1,6 +1,6 @@
 const flags = [
     { id: 1, property_id: 4, created_on: 02-07-2019, reason: "Weird demands", description: "problem description" },
     { id: 2, property_id: 3, created_on: 02-07-2019, reason: "Pricing", description: "Price is too high" },
-]
+];
 
 export default flags;
