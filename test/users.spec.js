4862b054de33aac27727cee3450f3a46d00b4abe
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2019-07-12 21:44:51 +0000

    Heroky deployment Settings

--- test/users.spec.js
+++ test/users.spec.js
@@ -1,16 +1,16 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../server/index';
-import users from '../server/models/User';
-import usersController from '../server/controllers/users';
-import propertiesValidator from '../server/MIDDLEWARE/properties';
-import Responding from '../server/helpers/responses';
-import sinon from 'sinon';
-const should = chai.should();
+// import chai from 'chai';
+// import chaiHttp from 'chai-http';
+// import app from '../server/index';
+// import users from '../server/models/User';
+// import usersController from '../server/controllers/users';
+// import propertiesValidator from '../server/MIDDLEWARE/properties';
+// import Responding from '../server/helpers/responses';
+// import sinon from 'sinon';
+// const should = chai.should();
 
 
-chai.use(chaiHttp);
-chai.should();
+// chai.use(chaiHttp);
+// chai.should();
 // describe('POST /', () => {
 //     it('New property, it should return 201', done => {
 //         chai.request(app)
